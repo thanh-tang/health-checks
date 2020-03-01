b0f5baae2579ecf6e017ee805d18dfb5379d7610
Thanh Tang <land.oscar@gmail.com>
2020-03-01 00:09:16 -0800

    Add new function Network Connectivity Check

--- all_checks.py
+++ all_checks.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import psutil
 import sys
+import socket
 
 def check_reboot():
   """Returns True if the computer has a pending reboot"""
@@ -23,6 +24,14 @@ def check_root_full():
   """Returns True if the root partition is full, False otherwise."""
   return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_no_network():
+  """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+      socket.gethostbyname("www.google.com")
+      return False
+    except:
+      return True
+
 def check_cpu_constrained():
   """Returns True if the cpu is having too much usage, False otherwise."""
   return psutil.cpu_percent(1) > 75
@@ -31,6 +40,7 @@ def main():
   checks=[
     (check_reboot, "Pending Reboot"),
     (check_root_full, "Root Partition full"),
+    (check_no_network, "No working network"),
   ]
   everything_ok = True
   for check, msg in checks:
