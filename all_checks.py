0e3620001e611abd6d2f271d54253ff1cd61f1be
Thanh Tang <land.oscar@gmail.com>
2020-02-29 23:26:20 -0800

    Add completely source

--- all_checks.py
+++ all_checks.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 import os
+import shutil
+import sys
 
 def check_reboot():
   """Returns True if the computer has a pending reboot"""
   return os.path.exists("/run/reboot-required")
 
-def print_str(text):
-  print(text)
+def check_disk_full(disk, min_gb, min_percent):
+  """Returns True if there isn't enough disk space, False otherwise."""
+  du = shutil.disk_usage(disk)
+  # Calculate the percentage of free space
+  percent_free = 100 * du.free / du.total
+  # Calculate how many free gigabytes
+  gigabytes_free = du.free / 2**30
+  if gigabytes_free < min_gb or percent_free < min_percent:
+    return True
+  return False
 
-def print_str2(text):
-  print(text)
+def check_root_full():
+  """Returns True if the root partition is full, False otherwise."""
+  return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
-def print_str3(text):
-  print(text)
-
-# main funtion
 def main():
-  if check_reboot():
-    print("Pending Reboot.")
-    sys.exit(1)
-  if disk_full_check():
-    print("Disk full")
+  checks=[
+    (check_reboot, "Pending Reboot"),
+    (check_root_full, "Root Partition full"),
+  ]
+  everything_ok = True
+  for check, msg in checks:
+    if check():
+      print(msg)
+      everything_ok = False
+  if not everything_ok:
     sys.exit(1)
   print("Everything is ok.")
   exit(0)
+
 main()
